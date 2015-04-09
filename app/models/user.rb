8a193913e5a6432ec7c7a969926fa1918d6ed67b
Justin LeFurjah <lefurjah@gmail.com>
2015-04-09 13:16:33 +0200

    added callback to downcase email before saving to database

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   validates :name, presence: true, length: {maximum: 50}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  before_save { self.email.downcase }
   validates :email, presence: true, length: {maximum: 255},
                     format: {with: VALID_EMAIL_REGEX  },
                     uniqueness: {case_sensitive: false}
