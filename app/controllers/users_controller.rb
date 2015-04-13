50e2932331b15d2de4ff5b6d5fca12a86ef9ec2a
Justin LeFurjah <lefurjah@gmail.com>
2015-04-13 18:42:43 +0200

    fixed new user save / redirect to view bug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,25 +2,17 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-
   end
 
   def new
-    flash[:success] = "Welcome to the Twitter-like demo rails app!"
     @user = User.new
-
-    if @user.save
-      redirect_to(@user)
-    else
-      render 'new'
-    end
-
   end
 
   def create
     @user = User.new(user_params)
     if @user.save
-      # Handle a successful save.
+      flash[:success] = "Welcome to the Sample Twitter-like Rails Demo App!"
+      redirect_to @user
     else
       render 'new'
     end
