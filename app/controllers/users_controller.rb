b9ac185ee8fa2407a254952d60f4ad1253cfb752
Justin LeFurjah <lefurjah@gmail.com>
2015-04-13 13:41:51 +0200

    added error message html, partial

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,5 +6,32 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
+
+    if @user.save
+      #handle successful save
+    else
+      render 'new'
+    end
+
   end
-end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # Handle a successful save.
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
+
+
+
+      end
