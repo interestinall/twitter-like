ec6e7e74f676e951cbe5198d1a0e321e2e0697ea
Justin LeFurjah <lefurjah@gmail.com>
2015-04-14 14:49:14 +0200

    got log out wrking, redirecting properly, with test

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,6 +2,11 @@ class SessionsController < ApplicationController
   def new
   end
 
+  def destroy
+    log_out
+    redirect_to root_url
+  end
+
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
@@ -15,8 +20,5 @@ class SessionsController < ApplicationController
   end
 
 
-  def destroy
-
-  end
 
 end
