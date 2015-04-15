db0597735d26a8557a1b7eec2fba02ca97ff41aa
Justin LeFurjah <lefurjah@gmail.com>
2015-04-15 11:59:29 +0200

    log in/out finished, inc testing + cookies

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,16 +3,19 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
   end
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-     log_in user
+      log_in user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       redirect_to user
+
     else
+
       # Create an error message.
       flash.now[:danger] = 'Invalid email or password. '
       render 'new'
